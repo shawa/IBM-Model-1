ff59009ebd5d567cc71325e5ab4ee258cb0c27ef
shawa <andrewshaw.mail@gmail.com>
2016-10-30 15:00:19 +0000

    add usage string

--- model_1.py
+++ model_1.py
@@ -28,44 +28,30 @@ def init_translation_probabilities(corpus):
 
 
 def train_iteration(corpus, words, total_s, prev_translation_probabilities):
-    # set count(e|f) to 0 for all e,f
     translation_probabilities = deepcopy(prev_translation_probabilities)
 
     counts = {word_en: {word_fr: 0 for word_fr in words['fr']}
               for word_en in words['en']}
-    # set total(f) to 0 for all f
+
     totals = {word_fr: 0 for word_fr in words['fr']}
 
-    # for all sentence pairs (e_s,f_s)
     for (es, fs) in [(pair['en'].split(), pair['fr'].split())
                      for pair in corpus]:
-        # for all words e in e_s
         for e in es:
-            # total_s(e) = 0
             total_s[e] = 0
 
-            # for all words f in f_s
             for f in fs:
-                # total_s(e) += t(e|f)
                 total_s[e] += translation_probabilities[e][f]
 
-        # for all words e in e_s
         for e in es:
-            # for all words f in f_s
             for f in fs:
-                # count(e|f) += t(e|f) / total_s(e)
                 counts[e][f] += (translation_probabilities[e][f] /
                                  total_s[e])
-
-                # total(f)   += t(e|f) / total_s(e)
                 totals[f] += translation_probabilities[e][f] / total_s[e]
 
-    # for all f
     for f in words['fr']:
-        # for all e
         for e in words['en']:
             translation_probabilities[e][f] = counts[e][f] / totals[f]
-        # t(e|f) = count(e|f) / total(f)
 
     return translation_probabilities
 
@@ -83,8 +69,12 @@ def train_model(corpus, epsilon):
     converged = False
     iterations = 0
     while not converged:
-        translation_probabilities = train_iteration(corpus, words, total_s,
-                                                    prev_translation_probabilities)
+        translation_probabilities = train_iteration(
+                                        # this is a disgusting way
+                                        # to indent code
+                                        corpus, words, total_s,
+                                        prev_translation_probabilities
+                                    )
 
         converged = is_converged(prev_translation_probabilities,
                                  translation_probabilities, epsilon)
@@ -94,6 +84,15 @@ def train_model(corpus, epsilon):
 
 
 def main(infile, *, epsilon:'e'=0.1):
+    '''
+    IBM Model 1 SMT Training Example
+
+    infile: json file containing english-french sentence pairs
+            in the form [ {"en": <sentence>, "fr": <sentence>}, ... ]
+
+    epsilon: acceptable euclidian distance between translation probability
+             vectors across iterations
+    '''
     corpus = get_corpus(infile)
     result, iterations = train_model(corpus, epsilon)
     pprint.pprint(result)
